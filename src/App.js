36bada7b10a850c11b7324f9f244d38783a0e8b8
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-30 18:18:46 +0900

    added style to components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Board from './components/Board';
 const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>TIC TAC TOE!</h1>
       <Board />
-    </>
+    </div>
   );
 };
 
