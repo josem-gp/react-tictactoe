4726ad69b1ee81b34275d4d19b2fd1481667b83d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-30 16:57:59 +0900

    added scss file for the design of the page and connected it to the project

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Square = ({ value }) => {
-  return <button type="button">{value}</button>;
+const Square = props => {
+  return <button type="button">{props.value}</button>;
 };
 
 export default Square;
