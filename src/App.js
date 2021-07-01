3792993c57d924c31e086be32d50cff404ce8001
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-01 15:22:55 +0900

    create a winning condition & message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
+
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
   const markSquare = position => {
     if (board[position]) {
       return;
@@ -22,6 +31,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE!</h1>
+      <h2>{message}</h2>
       <Board board={board} markSquare={markSquare} />
     </div>
   );
