3bf979c61d35b14b67bc72198ecbaad2ae30b7e5
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-01 15:12:14 +0900

    refactorize states in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+  const markSquare = position => {
+    if (board[position]) {
+      return;
+    }
+    setBoard(previous => {
+      return previous.map((square, idx) => {
+        if (idx === position) {
+          return isXNext ? 'X' : '0';
+        }
+        return square;
+      });
+    });
+
+    setIsXNext(previous => !previous);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE!</h1>
-      <Board />
+      <Board board={board} markSquare={markSquare} />
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  const markSquare = position => {
-    if (board[position]) {
-      return;
-    }
-    setBoard(previous => {
-      return previous.map((square, idx) => {
-        if (idx === position) {
-          return isXNext ? 'X' : '0';
-        }
-        return square;
-      });
-    });
-
-    setIsXNext(previous => !previous);
-  };
-
+const Board = ({ board, markSquare }) => {
   const renderSquare = position => {
     return (
       <Square
